681313d2d9c0436b402c6bfb678556c89a3866ec
GabrielRuderman <gabriel_ruderman@hotmail.com>
2018-08-03 02:15:21 -0300

    EL SABADO DUERMO SACADO

--- coordinador/src/coordinador.c
+++ coordinador/src/coordinador.c
@@ -636,7 +636,7 @@ t_control_configuracion cargarConfiguracion() {
 
 void finalizar(int cod) {
 	if (socketDeEscucha > 0) finalizarSocket(socketDeEscucha);
-	if (socketPlanificador > 0) finalizarSocket(socketDeEscucha);
+	if (socketPlanificador > 0) finalizarSocket(socketPlanificador);
 	log_destroy(logger_operaciones);
 	log_destroy(logger);
 	finalizarConexionArchivo(config);
